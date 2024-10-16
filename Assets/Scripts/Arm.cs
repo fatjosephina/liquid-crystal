23aa96fda9606abd7b1530b62354948f44bad965
Joseph Poselenzny <55261891+fatjosephina@users.noreply.github.com>
2024-10-16 00:39:46 -0500

    Creating more scripts

---
+++ Assets/Scripts/Arm.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arm : MonoBehaviour
+{
+    /* ArmTypes
+     * Punch
+     * Jump (Falcon)
+     * ShootNormal (Dragonfly)
+     * ShootStun (Dog)
+     */
+
+}
